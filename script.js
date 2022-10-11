e2eb29ff212de8d7975f7b07f7b23b5230a9eabe
regularwave <regularwave@gmail.com>
2022-10-11 14:46:26 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// const visCount = document.getElementById('visCount');
+// updateSiteCounter();
+
+
+
+// function updateSiteCounter() {
+//     fetch('https://api.countapi.xyz/hit/::UNIQUEKEY::')
+//         .then(response => response.json())
+//         .then(response => {
+//             visCount.innerHTML = response.value;
+//         })
+// }
